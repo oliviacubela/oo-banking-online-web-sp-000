50d96e7570a2677da77098ab48496ca2bd76a37d
Olivia Cubela <oliviacubela@gmail.com>
2020-07-14 14:06:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @balance = 1000
+    @status = "open"
+    @name = name
+  end
+
+  def deposit
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+
+  end
 end
